c0d7a13ab1c80916c1d23bee512a4e232c060b66
Zlata <st.zlataa@gmail.com>
2023-11-16 00:07:11 +0300

    modified info

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,8 @@ gendiff.name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
   .action((options) => {
     console.log(options.separator);
   });
