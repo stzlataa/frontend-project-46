ca1a4a98eed5d66bc774a56f240913d987684f4e
Zlata <st.zlataa@gmail.com>
2023-11-15 23:52:40 +0300

    first steps

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,4 +2,14 @@
 
 import { Command } from 'commander';
 
-const program = new Command();
+const gendiff = new Command();
+
+gendiff.name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .action((options) => {
+    console.log(options.separator);
+  });
+
+gendiff.parse();
