178d2c6e588d4e87e2fce2f81f3a96288b725a13
Zlata <st.zlataa@gmail.com>
2023-12-22 21:50:39 +0300

    fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,13 +8,14 @@ gendiff
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information')
+//   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2, options) => {
     const { format } = options;
     console.log(processFilepaths(filepath1, filepath2, format));
-  })
-  .parse(process.argv);
+  });
 
-// export default gendiff;
+gendiff.parse();
+
+export default gendiff;
