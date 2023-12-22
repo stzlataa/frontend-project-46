963d961b99bf71d7d1d194c06b538b2bed9b6495
Zlata <st.zlataa@gmail.com>
2023-12-22 22:27:32 +0300

    fix

---
+++ src/processFilepaths.js
@@ -0,0 +1,22 @@
+import genDiff from './genDiff.js';
+import { createFileInfo } from './utils.js';
+import * as path from 'node:path';
+import * as fs from 'node:fs';
+
+const processFilepaths = (filepath1, filepath2, format) => {
+  const filepaths = [filepath1, filepath2];
+
+  const fileDataArray = filepaths.map((filepath) => {
+    const resolvedPath = path.resolve(process.cwd(), filepath);
+
+    if (!fs.existsSync(resolvedPath)) {
+      console.log(`${resolvedPath} does not exist.`);
+      return false;
+    }
+    return createFileInfo(resolvedPath).data;
+  });
+
+  return genDiff(fileDataArray[0], fileDataArray[1], format);
+};
+
+export default processFilepaths;
---
+++ src/utils.js
@@ -0,0 +1,16 @@
+import * as path from 'node:path';
+import * as fs from 'node:fs';
+import parsers from './parsers.js';
+
+const getFileExtension = (filepath) => path.extname(filepath).slice(1);
+
+const createFileInfo = (file) => {
+  const extension = getFileExtension(file);
+  const data = parsers(fs.readFileSync(file, 'utf-8'), extension);
+
+  return { data, extension };
+};
+
+export {
+  getFileExtension, createFileInfo
+};
